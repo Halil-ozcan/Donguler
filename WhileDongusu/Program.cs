be093b7c185aecbcc17df9bffc666944129035d9
Halil Özcan <halil_ozcann@outlook.com>
2024-12-12 15:53:13 +0300

    C# WhileDongusu Yapıldı

--- WhileDongusu/Program.cs
+++ WhileDongusu/Program.cs
@@ -10,6 +10,46 @@ namespace WhileDongusu
     {
         static void Main(string[] args)
         {
+            int sayac = 1;
+            while (sayac <= 10)
+            {
+                Console.WriteLine(sayac);
+            }
+
+            Console.WriteLine("Lütfen Bir sayı Giriniz");
+            string kullaniciDeger = Console.ReadLine();
+
+            int sayac1 = int.Parse(kullaniciDeger);
+
+            while(sayac1 !=-1)
+            {
+                Console.WriteLine(sayac1);
+                sayac1--;
+            }
+
+
+            Console.Clear();
+
+            Console.WriteLine("Lütfen toplamak istedğiniz sayilari giriniz");
+            Console.WriteLine("Not : -1 değeri işlemlerinizi durdurur.");
+
+
+            int ornekKullanimToplam = 0;
+            while(true)
+            {
+                string KullaniciGelenDeger = Console.ReadLine();
+                int kullaniciDegerİnt = int.Parse(KullaniciGelenDeger);
+                if(kullaniciDegerİnt < 0)
+                {
+                    break;
+                }
+                else
+                {
+                    ornekKullanimToplam = ornekKullanimToplam + kullaniciDegerİnt;
+                }
+            }
+
+            Console.WriteLine("Toplam deger : {0}", ornekKullanimToplam);
         }
     }
 }
