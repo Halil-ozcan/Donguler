8c9cc5c267d2932423b83b83d99879321dae9382
Halil Özcan <halil_ozcann@outlook.com>
2024-12-12 16:15:24 +0300

    C# While Dongusu Alistirma Yapıldı

--- WhileDongusuAlistirma/Program.cs
+++ WhileDongusuAlistirma/Program.cs
@@ -10,6 +10,31 @@ namespace WhileDongusuAlistirma
     {
         static void Main(string[] args)
         {
+            int tahminAdet = 1;
+            int sistemdekiSayi = 0;
+            Random rd = new Random();
+            sistemdekiSayi = rd.Next(1, 10);
+
+            while(true)
+            {
+                Console.Write("{0} . deneme Lütfen sistemin Üretmiş olduğu sayıyı Tahmin ediniz : ", tahminAdet);
+                string rdmKullaniciGelen = Console.ReadLine();
+
+                int rdmKullaniciGelenİnt = int.Parse(rdmKullaniciGelen);
+
+                if (rdmKullaniciGelenİnt == sistemdekiSayi)
+                {
+                    tahminAdet++;
+                    Console.WriteLine("{0}. denemenizde Buldunuz. Tebrikler", tahminAdet);
+                    break;
+                }
+                else
+                {
+                    Console.Clear();
+                    Console.WriteLine("{0}. denemeniz Tahmin edemediniz Lütfen Tekrar Deneyiniz", tahminAdet);
+                    tahminAdet++;
+                }
+            }
         }
     }
 }
