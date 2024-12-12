bb3647ef8f873e0f20e9d8f73bd92d1adf31d688
Halil Özcan <halil_ozcann@outlook.com>
2024-12-12 14:44:27 +0300

    C# For Dongusu Yapıdlı.

--- Donguler/Program.cs
+++ Donguler/Program.cs
@@ -10,6 +10,28 @@ namespace Donguler
     {
         static void Main(string[] args)
         {
+            for (int i = 1; i <=10; i++)
+            {
+                Console.WriteLine(i + ". tekrar !");
+            }
+
+            for(int sayac = 1; sayac<=100; sayac++)
+            {
+                if(sayac % 2 == 0)
+                {
+                    Console.WriteLine(sayac + " - çift Sayıdır.");
+                }
+            }
+
+            int toplam = 0;
+            for(int sayac = 1; sayac<=100; sayac++)
+            {
+                if(sayac % 2 == 0)
+                {
+                    toplam = toplam + sayac;
+                }
+            }
+            Console.WriteLine("toplam : {0}", toplam);
         }
     }
 }
