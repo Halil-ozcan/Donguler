564d068bbc06b0b2563bdf35562bd8496b71cb90
Halil Özcan <halil_ozcann@outlook.com>
2024-12-12 14:55:48 +0300

    C# For dongusu Alistirması Yapıldı

---
+++ ForAlistirma/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ForAlistirma
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string KullaniciGelenDeger = string.Empty;
+            int kullaniciDegerİnt = 0;
+            int faktoriyelHesapla = 1;
+
+            Console.WriteLine("Lütfen Faktoriyel Hesabı Yapmak İstediğiniz değeri Yazınız");
+            KullaniciGelenDeger = Console.ReadLine();
+
+            kullaniciDegerİnt = int.Parse(KullaniciGelenDeger);
+
+            for (int i = kullaniciDegerİnt; i > 1; i--)
+            {
+                faktoriyelHesapla = faktoriyelHesapla * i; 
+            }
+            Console.WriteLine("{0} değerinin faktoriyel Sonucu : {1}", KullaniciGelenDeger, faktoriyelHesapla);
+        }
+    }
+}
---
+++ ForAlistirma/Properties/AssemblyInfo.cs
@@ -0,0 +1,33 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// Bir bütünleştirilmiş koda ilişkin Genel Bilgiler aşağıdaki öznitelikler kümesiyle
+// denetlenir. Bütünleştirilmiş kod ile ilişkili bilgileri değiştirmek için
+// bu öznitelik değerlerini değiştirin.
+[assembly: AssemblyTitle("ForAlistirma")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("HP")]
+[assembly: AssemblyProduct("ForAlistirma")]
+[assembly: AssemblyCopyright("Copyright © HP 2024")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// ComVisible özniteliğinin false olarak ayarlanması bu bütünleştirilmiş koddaki türleri
+// COM bileşenleri için görünmez yapar. Bu bütünleştirilmiş koddaki bir türe
+// erişmeniz gerekirse ComVisible özniteliğini o türde true olarak ayarlayın.
+[assembly: ComVisible(false)]
+
+// Bu proje COM'un kullanımına sunulursa, aşağıdaki GUID tür kitaplığının kimliği içindir
+[assembly: Guid("d1149583-5f89-4697-a070-ac5c2b6ed923")]
+
+// Bir derlemenin sürüm bilgileri aşağıdaki dört değerden oluşur:
+//
+//      Ana Sürüm
+//      İkincil Sürüm 
+//      Yapı Numarası
+//      Düzeltme
+//
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
